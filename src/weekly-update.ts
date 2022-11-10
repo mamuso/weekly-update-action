566e3983065f80830cc20842306aa8a7c7665e14
Manuel Muñoz Solera <mamuso@mamuso.net>
2022-11-09 21:23:57 -0800

    Adding switch-case routes

--- src/weekly-update.ts
+++ src/weekly-update.ts
@@ -8,6 +8,7 @@ import type {configuration} from './types'
 
 export default class weeklyUpdate {
   configuration: configuration
+  route: string
   today: string
   executedToday: boolean
 
@@ -18,6 +19,7 @@ export default class weeklyUpdate {
       advance_on: userConfiguration.advance_on || null,
       remind_on: userConfiguration.remind_on || 'Thu'
     }
+    this.route = ''
     this.today = new Date().toLocaleDateString('en-US', {
       weekday: 'short'
     })
@@ -30,15 +32,38 @@ export default class weeklyUpdate {
       // Check if the action should be executed today
       if (
         [
-          this.configuration.post_on,
           this.configuration.advance_on,
+          this.configuration.post_on,
           this.configuration.remind_on
         ].includes(this.today)
       ) {
+        // eslint-disable-next-line no-console
+        console.log(this.today)
+
+        switch (this.today) {
+          case this.configuration.advance_on:
+            // Advance the week
+            this.route = 'advance'
+            // eslint-disable-next-line no-console
+            console.log('Advance the week')
+            break
+          case this.configuration.post_on:
+            // Post the weekly update
+            this.route = 'post'
+            // eslint-disable-next-line no-console
+            console.log('Post the weekly update')
+            break
+          case this.configuration.remind_on:
+            // Remind the team to post the weekly update
+            this.route = 'remind'
+            // eslint-disable-next-line no-console
+            console.log('Remind the team to post the weekly update')
+            break
+        }
+
+        // Mark the action as executed today
         this.executedToday = true
       }
-      // eslint-disable-next-line no-console
-      console.log(this.configuration)
     } catch (error) {
       if (error instanceof Error) {
         core.setFailed(error.message)
--- tests/weekly-update.test.ts
+++ tests/weekly-update.test.ts
@@ -16,7 +16,24 @@ describe('WeeklyUpdate test suite', () => {
     jest.restoreAllMocks()
   })
 
-  it('should run the action if post_on, advance_on or remind_on are not null', async () => {
+  it('should not run if post_on, advance_on or remind_on are not Today', async () => {
+    const configuration: configuration = {
+      post_on: 'Mon',
+      advance_on: null,
+      remind_on: 'Thu'
+    }
+    const weeklyUpdateAction = new weeklyUpdate(configuration)
+    // intercept Today's date
+    weeklyUpdateAction.today = 'Tue'
+
+    weeklyUpdateAction.run()
+
+    expect(weeklyUpdateAction.configuration).toEqual(configuration)
+    expect(weeklyUpdateAction.today).toEqual('Tue')
+    expect(weeklyUpdateAction.executedToday).toEqual(false)
+  })
+
+  it('should run if post_on, advance_on or remind_on are are Today', async () => {
     const configuration: configuration = {
       post_on: 'Mon',
       advance_on: null,
@@ -30,6 +47,43 @@ describe('WeeklyUpdate test suite', () => {
 
     expect(weeklyUpdateAction.configuration).toEqual(configuration)
     expect(weeklyUpdateAction.today).toEqual('Mon')
+    expect(weeklyUpdateAction.route).toEqual('post')
+    expect(weeklyUpdateAction.executedToday).toEqual(true)
+  })
+
+  it('should run advance if post_on, advance_on are equal', async () => {
+    const configuration: configuration = {
+      post_on: 'Tue',
+      advance_on: 'Tue',
+      remind_on: 'Thu'
+    }
+    const weeklyUpdateAction = new weeklyUpdate(configuration)
+    // intercept Today's date
+    weeklyUpdateAction.today = 'Tue'
+
+    weeklyUpdateAction.run()
+
+    expect(weeklyUpdateAction.configuration).toEqual(configuration)
+    expect(weeklyUpdateAction.today).toEqual('Tue')
+    expect(weeklyUpdateAction.route).toEqual('advance')
+    expect(weeklyUpdateAction.executedToday).toEqual(true)
+  })
+
+  it('should run remind if remind_on is Today', async () => {
+    const configuration: configuration = {
+      post_on: 'Tue',
+      advance_on: 'Tue',
+      remind_on: 'Thu'
+    }
+    const weeklyUpdateAction = new weeklyUpdate(configuration)
+    // intercept Today's date
+    weeklyUpdateAction.today = 'Thu'
+
+    weeklyUpdateAction.run()
+
+    expect(weeklyUpdateAction.configuration).toEqual(configuration)
+    expect(weeklyUpdateAction.today).toEqual('Thu')
+    expect(weeklyUpdateAction.route).toEqual('remind')
     expect(weeklyUpdateAction.executedToday).toEqual(true)
   })
 })
