b8a4dd6171b10b579da586b2de7dce83138504f4
Manuel Muñoz Solera <mamuso@mamuso.net>
2022-12-09 22:16:28 -0800

    changing process for getinput

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import weeklyUpdate from './weekly-update'
+import * as core from '@actions/core'
 import type {configuration} from './types'
 
 const actionConfiguration: configuration = {
-  post_on: process.env.post_on,
-  advance_on: process.env.advance_on,
-  remind_on: process.env.remind_on,
-  title: process.env.title,
-  post_template: process.env.post_template,
-  remind_template: process.env.remind_template,
-  repo: process.env.repo
+  post_on: core.getInput('post_on'),
+  advance_on: core.getInput('advance_on'),
+  remind_on: core.getInput('remind_on'),
+  title: core.getInput('title'),
+  post_template: core.getInput('post_template'),
+  remind_template: core.getInput('remind_template'),
+  repo: core.getInput('repo')
 }
 const weekly = new weeklyUpdate(actionConfiguration)
 weekly.run()
